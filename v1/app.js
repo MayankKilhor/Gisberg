6471e108b803ff38daa9b1d2968a35ca15338bf7
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2020-08-18 06:43:51 +0530

    Update app.js

--- v1/app.js
+++ v1/app.js
@@ -2,9 +2,16 @@ var express= require("express");
 var app = express();
 var PORT = 3000; 
   
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.send("this will be landing page soon!!");
+    res.render("landing");
+   
 });
+
+app.get("/campgrounds",function(req,res){
+    
+})
 
 app.listen(PORT, function(err){ 
     if (err) console.log("Error in server setup") 
