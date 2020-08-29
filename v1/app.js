0c3f46e503e4ee27218de5a1b8d12e01a57889e9
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2020-08-29 06:17:56 +0530

    Update app.js

--- v1/app.js
+++ v1/app.js
@@ -1,21 +1,22 @@
 var express= require("express");
 var app = express();
-var PORT = 3000; 
-  
-app.set("view engine","ejs");
 
-app.get("/",function(req,res){
-    res.render("landing");
-   
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
 
-app.get("/campgrounds",function(req,res){
-    
-})
+app.set("view engine","ejs");
 
-app.listen(PORT, function(err){ 
-    if (err) console.log("Error in server setup") 
-    console.log("Server listening on Port", PORT); 
-})
+app.get("/",function(req, res){
+    res.render("landing");   
+});
 
-
+app.get("/campgrounds",function(req, res){
+    var campgrounds=[
+        {name:"Kadal",image: "https://images.pexels.com/photos/4268140/pexels-photo-4268140.jpeg?auto=compress&cs=tinysrgb&h=350"},
+        {name:"Goa",image: "https://pixabay.com/get/54e5d4414356a814f1dc84609620367d1c3ed9e04e5074407d2a7dd6964ecd_340.jpg"}
+    ]
+    res.render("campgrounds",{Campgrounds:campgrounds});
+});
